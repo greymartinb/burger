e42d0fd465b390c3e15b1e68efff1a2100d08a3e
greymartinb <greybuhrdorf@Greys-MacBook-Pro.local>
2017-04-28 10:07:15 -0500

    should work

--- config/connection.js
+++ config/connection.js
@@ -10,6 +10,11 @@ var connection = mysql.createConnection({
 });
 
 // Make connection.
+if(process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,6 +22,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+}
 
 // Export connection for our ORM to use.
 module.exports = connection;
